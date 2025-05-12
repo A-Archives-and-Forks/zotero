2f38926da532b510d210295f395ac5169cbb8893
Abe Jellinek <jellinek@berkeley.edu>
2025-05-12 12:45:55 -0400

    CasePreservingHeaders: Fix some Headers instances not being recognized
    I guess the Headers class accessible from test code is different from
    the Headers class in http.js (for the purposes of instanceof, at least).

--- chrome/content/zotero/xpcom/http.js
+++ chrome/content/zotero/xpcom/http.js
@@ -1704,7 +1704,7 @@ Zotero.HTTP = new function () {
 			super();
 			if (init) {
 				let iter;
-				if (Array.isArray(init) || init instanceof Headers) {
+				if (Symbol.iterator in init) {
 					iter = init;
 				}
 				else {
